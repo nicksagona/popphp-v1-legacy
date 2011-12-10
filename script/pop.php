23838a7d62acb6be8a3d783234f738a5c828f5d9
Nick Sagona <nick@moc10media.com>
2011-12-09 22:02:07 -0600

    Work on Version and CLI script

--- script/pop.php
+++ script/pop.php
@@ -1,4 +1,46 @@
 #!/usr/bin/php
 <?php
+/**
+ * Pop PHP Framework PHP CLI script
+ *
+ * LICENSE
+ *
+ * This source file is subject to the new BSD license that is bundled
+ * with this package in the file LICENSE.TXT.
+ * It is also available through the world-wide-web at this URL:
+ * http://www.popphp.org/LICENSE.TXT
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to info@popphp.org so we can send you a copy immediately.
+ *
+ * Possible arguments
+ *
+ * -v --version    Display version of Pop PHP Framework
+ * -c --check      Check the current configuration for required dependencies
+ * -b --build      Build a project based on the project configuration file
+ *
+ */
+require_once __DIR__ . '/../public/bootstrap.php';
 
-echo 'This will be the Pop PHP Framework PHP CLI file.' . PHP_EOL;
+if (isset($argv[1])) {
+    // Write header
+    echo PHP_EOL . 'Pop PHP Framework PHP CLI script' . PHP_EOL;
+    echo '================================' . PHP_EOL . PHP_EOL;
+
+    // Check for version
+    if (($argv[1] == '-v') || ($argv[1] == '--version')) {
+        echo 'Version Check' . PHP_EOL;
+        echo '-------------' . PHP_EOL;
+        echo 'Installed: ' . Pop\Version::getVersion() . PHP_EOL;
+        echo 'Latest Available: ' . Pop\Version::getLatest() . PHP_EOL;
+    // Else, check dependencies
+    } else if (($argv[1] == '-c') || ($argv[1] == '--check')) {
+        echo 'Dependencies Check' . PHP_EOL;
+        echo '------------------' . PHP_EOL;
+        echo Pop\Version::check() . PHP_EOL;
+    // Else, build project
+    } else if (($argv[1] == '-b') || ($argv[1] == '--build')) {
+        echo 'Build';
+    }
+
+}
--- vendor/PopPHPFramework/src/Pop/Version.php
+++ vendor/PopPHPFramework/src/Pop/Version.php
@@ -46,6 +46,21 @@ class Version
     const URL = 'http://www.popphp.org/version.txt';
 
     /**
+     * Return plain text string
+     */
+    const PLAIN = 1;
+
+    /**
+     * Return html formatted string
+     */
+    const HTML = 2;
+
+    /**
+     * Return the data as a PHP array
+     */
+    const DATA = 3;
+
+    /**
      * Returns the version of this library install.
      *
      * @return string
@@ -82,6 +97,114 @@ class Version
         }
 
         return $latest;
+    }
+
+    /**
+     * Returns an output of dependencies
+     *
+     * @param int $ret
+     * @return string|array
+     */
+    public static function check($ret = self::PLAIN)
+    {
+        $php = array();
+        $check = array();
+
+        // PHP Version
+        $php['Required PHP'] = '5.3.0';
+        $php['Installed PHP'] = PHP_VERSION . ((version_compare(PHP_VERSION, '5.3.0') >= 0) ? ' (Pass)' : ' (Fail)');
+
+        // Archive
+        $check['Archive Tar'] = (class_exists('Archive_Tar')) ? 'Yes' : 'No';
+        $check['Archive Phar'] = (class_exists('Phar')) ? 'Yes' : 'No';
+        $check['Archive Rar'] = (class_exists('RarArchive')) ? 'Yes' : 'No';
+        $check['Archive Zip'] = (class_exists('ZipArchive')) ? 'Yes' : 'No';
+
+        // Compress
+        $check['Compress Bzip2'] = (function_exists('bzcompress'))  ? 'Yes' : 'No';
+        $check['Compress Lzf'] = (function_exists('lzf_compress'))  ? 'Yes' : 'No';
+        $check['Compress Zlib'] = (function_exists('gzcompress'))  ? 'Yes' : 'No';
+
+        // cURL
+        $check['cURL'] = (function_exists('curl_init'))  ? 'Yes' : 'No';
+
+        // DB
+        $check['Db MySql'] = (function_exists('mysql_connect'))  ? 'Yes' : 'No';
+        $check['Db MySqli'] = (class_exists('mysqli')) ? 'Yes' : 'No';
+        $check['Db Pdo'] = (class_exists('Pdo'))  ? 'Yes' : 'No';
+        $check['Db PgSql'] = (function_exists('pg_connect'))  ? 'Yes' : 'No';
+        $check['Db Sqlite'] = (class_exists('Sqlite3'))  ? 'Yes' : 'No';
+
+        // DOM/XML
+        $check['Dom DOMDocument'] = (class_exists('DOMDocument'))  ? 'Yes' : 'No';
+        $check['Dom SimpleXml'] = (class_exists('SimpleXMLElement'))  ? 'Yes' : 'No';
+
+        // FTP
+        $check['FTP'] = (function_exists('ftp_connect'))  ? 'Yes' : 'No';
+
+        // Image
+        $check['Image Gd'] = (function_exists('getimagesize'))  ? 'Yes' : 'No';
+        $check['Image Freetype'] = (function_exists('imagettftext'))  ? 'Yes' : 'No';
+        $check['Image Imagick'] = (class_exists('Imagick'))  ? 'Yes' : 'No';
+
+        // Memcache
+        $check['Memcache'] = (class_exists('Memcache'))  ? 'Yes' : 'No';
+
+        // Total
+        $count = array_count_values($check);
+        $total = count($check) - $count['No'] . ' of ' . count($check);
+
+        $results = null;
+        switch ($ret) {
+            case 1:
+                foreach ($php as $key => $value) {
+                    $results .= $key . ': ' . $value . PHP_EOL;
+                }
+                $results .= '------------------' . PHP_EOL;
+                foreach ($check as $key => $value) {
+                    $results .= $key . ': ' . $value . PHP_EOL;
+                }
+                $results .= '------------------' . PHP_EOL;
+                $results .= 'Total: ' . $total . PHP_EOL;
+                break;
+            case 2:
+                foreach ($php as $key => $value) {
+                    $color = (stripos($value, 'fail') !== false) ? 'red' : 'green';
+                    $results .= '<strong>' . $key . ':</strong> <span style="color: ' . $color . ';">' . $value . '</span><br />' . PHP_EOL;
+                }
+                $results .= '<hr />' . PHP_EOL;
+                foreach ($check as $key => $value) {
+                    $color = ($value == 'No') ? 'red' : 'green';
+                    $results .= '<strong>' . $key . ':</strong> <span style="color: ' . $color . ';">' . $value . '</span><br />' . PHP_EOL;
+                }
+                $results .= '<hr />' . PHP_EOL;
+                $results .= '<strong>Total:</strong> ' . $total . PHP_EOL;
+                break;
+            case 3:
+                $data = array();
+                foreach ($php as $key => $value) {
+                    if (strpos($key, ' ') !== false) {
+                        $k = str_replace(' ', '', $key);
+                        $k = strtolower(substr($k, 0, 1)) . substr($k, 1);
+                    } else {
+                        $k = strtolower($key);
+                    }
+                    $data[$k] = $value;
+                }
+                foreach ($check as $key => $value) {
+                    if (strpos($key, ' ') !== false) {
+                        $k = str_replace(' ', '', $key);
+                        $k = strtolower(substr($k, 0, 1)) . substr($k, 1);
+                    } else {
+                        $k = strtolower($key);
+                    }
+                    $data[$k] = $value;
+                }
+                $results = new \ArrayObject($data, \ArrayObject::ARRAY_AS_PROPS);
+                break;
+        }
+        return $results;
+
     }
 
 }
