2deedefe490dbce52bcf6878e27c44035fc7b6f3
Nick Sagona <nick@moc10media.com>
2013-03-19 13:19:50 -0500

    Work on refactor/acl examples

---
+++ public/examples/auth/acl.php
@@ -0,0 +1,13 @@
+<?php
+
+require_once '../../bootstrap.php';
+
+use Pop\Auth\Acl;
+use Pop\Auth\Role;
+use Pop\Auth\Resource;
+
+try {
+
+} catch (\Exception $e) {
+    echo $e->getMessage() . PHP_EOL . PHP_EOL;
+}
--- public/examples/auth/roles.php
+++ public/examples/auth/roles.php
@@ -5,17 +5,20 @@ require_once '../../bootstrap.php';
 use Pop\Auth\Role;
 
 try {
+    // Create some roles with permissions
     $reader = Role::factory('reader')->addPermission('read');
     $editor = Role::factory('editor')->addPermission('edit');
     $publisher = Role::factory('publisher')->addPermission('publish');
     $admin = Role::factory('admin')->addPermission('admin');
 
+    // Add roles as child roles to demonstrate inheritance
     $reader->addChild(
         $editor->addChild(
             $publisher->addChild($admin)
         )
     );
 
+    // Editor can read and edit, but not publish
     echo ($editor->hasPermission('publish')) ? 'Yes' : 'No';
 } catch (\Exception $e) {
     echo $e->getMessage() . PHP_EOL . PHP_EOL;
