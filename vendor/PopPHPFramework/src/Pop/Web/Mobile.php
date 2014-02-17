151788290400ad665428783bbf56b13eaa3e5336
Nick Sagona <nick@moc10media.com>
2014-02-16 21:33:09 -0600

    Update the Web\Mobile class

--- vendor/PopPHPFramework/src/Pop/Web/Mobile.php
+++ vendor/PopPHPFramework/src/Pop/Web/Mobile.php
@@ -131,7 +131,7 @@ class Mobile
     public function __construct($mobile = null, $desktop = null, $force = 0)
     {
         // Set the user agent and object properties.
-        $this->ua = (isset($_SERVER['HTTP_USER_AGENT'])) ? $_SERVER['HTTP_USER_AGENT'] : 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:16.0) Gecko/20100101 Firefox/16.0';
+        $this->ua = (isset($_SERVER['HTTP_USER_AGENT'])) ? $_SERVER['HTTP_USER_AGENT'] : null;
         $this->mobileUrl = $mobile;
         $this->desktopUrl = $desktop;
         $this->mobile = $this->detect();
